91fa388bfd281d87ca414943b0a2b4c1d7ee4850
samirasingh19 <samira.singh021@gmail.com>
2023-04-08 09:38:03 +0530

    Day10 AddressBookProblem

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Day10AddressBook
+{
+    class Program
+    {
+
+        public string firstname, lastname, mobilenumber, address, city, state, zip, email;
+        public static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello, Welcome to the Adress Book Program: ");
+            //ContactManagementUC1.CreateContact();
+
+            ContactManagementUC3 obj = new ContactManagementUC3();
+            obj.Usecase3_ContactManagement();
+
+        }
+
+    }
+
+
+}
