d1bc497d03a66d3325b90d06a140e8ace1efd89b
samirasingh19 <samira.singh021@gmail.com>
2023-04-11 19:13:47 +0530

    ContactManagement

---
+++ ContactManagement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10AddressBookProblem
+{
+    public class ContactManagement
+    {
+        public static void CreateContact()
+        {
+            string firstname;
+            string lastname;
+            string address;
+            string city;
+            string state;
+            string zip;
+
+            string phone;
+            string email;
+            string phoneNumber;
+        }
+    }
+}
